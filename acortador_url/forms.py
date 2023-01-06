dd218a65ce1288bb8fbf70cd9effef52a34ef991
Darlin Ruben Nina C <chinausto@gmail.com>
2023-01-05 23:12:27 -0400

    js

--- acortador_url/forms.py
+++ acortador_url/forms.py
@@ -4,7 +4,7 @@ class genericUrl(forms.ModelForm):
 
     url = forms.URLField(label='url', required=True,
                          widget=forms.TextInput(attrs={'placeholder': 'url', 'class':'form-control'}))
-    acor_url = forms.CharField(label='nombre de la url', required=True, max_length=11,
+    acor_url = forms.CharField(label='nombre de la url', required=True, max_length=18,
                                widget=(forms.TextInput(attrs={'placeholder': 'Nombre de la url', 'class':'form-control'})))
     class Meta:
         model = acortador_url
--- acortador_url/static/JS/index.js
+++ acortador_url/static/JS/index.js
@@ -1,6 +1,14 @@
 const btn_qr = document.querySelectorAll(".btn_qr");
 const QR = document.querySelector(".QR");
 const contents_qr = new QRCode(QR);
+const btn_url = [...document.getElementsByClassName("btn_url")];
+const a_url = document.getElementById("a_url");
+
+btn_url.forEach((element) => {
+  element.addEventListener("click", () => {
+    window.open((location.href = element.value));
+  });
+});
 
 btn_qr.forEach((element) => {
   element.addEventListener("click", () => {
