99ed9b08a8827875db359546f299e83dcc5d12e2
Darlin Ruben Nina C. <chinausto@gmail.com>
2023-10-28 02:55:27 -0400

    arreglar bugs

--- config/settings.py
+++ config/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRETKEY')
+SECRET_KEY = "django-insecure-5e&@a!45)26n0(==gd2#ek%)9scq1i_!%ep66iynegg_gl_az!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app',]
 
